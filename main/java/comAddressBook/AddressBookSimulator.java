a8822bd3a2e7022b5767cfe159c1ec9f9420d683
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-04-19 19:10:49 +0530

    REFACTORED: Able to add multiple address books

--- main/java/comAddressBook/AddressBookSimulator.java
+++ main/java/comAddressBook/AddressBookSimulator.java
@@ -1,9 +1,68 @@
 package comAddressBook;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddressBookSimulator {
+    Map<String, AddressBook> booksMap = new HashMap<>();
+    ScannerForAddressBook scannerForAddressBook = initializeScanner();
+    static AddressBookSimulator addressBookSimulator = new AddressBookSimulator();
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Simulator program!");
-        AddressBook addressBook = new AddressBook();
-        addressBook.accessAddressBook();
+        ScannerForAddressBook scannerForAddressBook = initializeScanner();
+
+        System.out.println("Welcome to address book simulator!");
+
+        boolean isExit = false;
+        while(!isExit) {
+            System.out.println("Select options: \n1.Add Book\n2.AccessBook\n3.Exit");
+            int option = scannerForAddressBook.scannerProvider().nextInt();
+            switch(option) {
+                case 1:
+                    addressBookSimulator.addBook();
+                    break;
+                case 2:
+                    addressBookSimulator.accessBook();
+                    break;
+                default:
+                    isExit = true;
+                    System.out.println("Thanks for using Address Book Simulator!");
+                    scannerForAddressBook.scannerProvider().close();//closing scanner
+            }
+        }
+    }
+
+    /**
+     * add new Book
+     */
+    public void addBook(){
+        System.out.println("Enter the name of new book");
+        String bookName = scannerForAddressBook.scannerProvider().nextLine();
+        if(addressBookSimulator.booksMap.containsKey(bookName)){
+            System.out.println("Book already exists!");
+        } else {
+            addressBookSimulator.booksMap.put(bookName, new AddressBook());
+        }
+    }
+
+    /**
+     * Access existing Book
+     */
+    public void accessBook(){
+        System.out.println("Enter the name of the book to access it");
+        Object bookName1 = scannerForAddressBook.scannerProvider().nextLine();
+        if(addressBookSimulator.booksMap.containsKey(bookName1)) {
+            AddressBook addressBook = addressBookSimulator.booksMap.get(bookName1);
+            addressBook.accessAddressBook();
+            System.out.println("sorted contacts: "+addressBookSimulator.booksMap.toString());
+        }
+    }
+
+    /**
+     * initializes scanner class
+     */
+    public static ScannerForAddressBook initializeScanner(){
+        ScannerForAddressBook scannerForAddressBook = new ScannerForAddressBook();
+        return scannerForAddressBook;
     }
 }
